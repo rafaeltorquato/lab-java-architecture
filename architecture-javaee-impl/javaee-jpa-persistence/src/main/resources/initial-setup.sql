7d38474dde0b724531833fe55c07b17ba6224320
Rafael Torquato <rafael.araujodf@gmail.com>
2018-06-07 09:06:19 -0300

    Fix initial inserts for javaee-jpa-architecture

--- architecture-javaee-impl/javaee-jpa-persistence/src/main/resources/initial-setup.sql
+++ architecture-javaee-impl/javaee-jpa-persistence/src/main/resources/initial-setup.sql
@@ -1,7 +1,3 @@
-insert into HIRERENTITY(SSN, BIRTHDATE)
-  select '123456789', '1982-10-07'
-  where not exists(select null from HIRERENTITY where ssn = '123456789');
+insert into HIRERENTITY(SSN, BIRTHDATE) values ('123456789', '1982-10-07');
 
-insert into HIRERENTITY(SSN, BIRTHDATE, DATEOFDEATH)
-  select '987654321', '2000-01-01', '2018-01-01'
-  where not exists(select null from HIRERENTITY where ssn = '987654321');
+insert into HIRERENTITY(SSN, BIRTHDATE, DATEOFDEATH) values('987654321', '2000-01-01', '2018-01-01');
